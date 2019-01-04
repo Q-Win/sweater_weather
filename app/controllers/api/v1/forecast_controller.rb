7710a7af016c158dda53ef8a85ddf08a720f8e3b
Quinn Krug <quinn.krug@gmail.com>
2019-01-03 17:33:35 -0700

    user story 1 done, I think

--- app/controllers/api/v1/forecast_controller.rb
+++ app/controllers/api/v1/forecast_controller.rb
@@ -2,7 +2,6 @@ class Api::V1::ForecastController < ApplicationController
 
   def index
     forecast = ForecastData.new(params[:location])
-    # render json: forecast.get_forecast
     render json: ForecastDataSerializer.new(forecast)
 
   end
--- spec/models/forecast_spec.rb
+++ spec/models/forecast_spec.rb
@@ -7,4 +7,14 @@ describe ForecastData do
     expect(subject).to be_a(ForecastData)
   end
 
+  it 'can get a forecast' do
+    VCR.use_cassette("forecast_data_cassette") do
+      forecast = ForecastData.new("Denver")
+      expect(forecast.currently).to be_a(Hash)
+      expect(forecast.hourly).to be_a(Hash)
+      expect(forecast.daily).to be_a(Hash)
+      expect(forecast.json_id).to be_a(String)
+    end
+  end
+
 end
