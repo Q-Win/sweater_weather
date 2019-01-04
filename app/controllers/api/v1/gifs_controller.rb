28fde964b438d13f4764fdd0d72fa45ed0bd4bba
Quinn Krug <quinn.krug@gmail.com>
2019-01-04 12:04:30 -0700

    copyright

--- app/controllers/api/v1/gifs_controller.rb
+++ app/controllers/api/v1/gifs_controller.rb
@@ -1,7 +1,11 @@
 class Api::V1::GifsController < ApplicationController
 
   def index
-    forecast = GifBuilder.new(params[:location]).forecast  
-    render json: DailyForecastSerializer.new(forecast)
+    forecast = GifBuilder.new(params[:location]).forecast
+    render json: DailyForecastSerializer.new(forecast), message: {
+        copyright: "2018"
+    }
+
+
   end
 end
--- app/serializers/gif_builder_serializer.rb
+++ app/serializers/gif_builder_serializer.rb
@@ -1,5 +1,4 @@
 class GifBuilderSerializer
   include FastJsonapi::ObjectSerializer
-  set_id :json_id
   attributes :forecast_gif_json
 end
