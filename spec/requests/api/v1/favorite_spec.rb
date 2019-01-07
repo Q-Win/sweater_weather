58cb5866e7f3810c8acffe834f0a05691bc010bd
Quinn Krug <quinn.krug@gmail.com>
2019-01-07 15:50:19 -0700

    test created

--- spec/requests/api/v1/favorite_spec.rb
+++ spec/requests/api/v1/favorite_spec.rb
@@ -14,16 +14,30 @@ describe "favorites request" do
     end
   end
 
-  it "favorite request with valid api key " do
+  it "favorite request with invalid api key " do
     VCR.use_cassette("unsuccessful_favorite_cassette") do
       user = User.create(email: 'bob', password: 'cat', api_key: "testkey")
 
       post '/api/v1/favorites?location=denver&api_key=wrongkey'
 
       error = JSON.parse(response.body)
-      
+
       expect(user.favorites.count).to eq(0)
       expect(error['error']).to eq("Incorrect API key")
+    end
+  end
+
+  it "lists favorites request with invalid api key " do
+    VCR.use_cassette("successful_favorite_get_cassette") do
+      user = User.create(email: 'bob', password: 'cat', api_key: "testkey")
+      favorite = user.favorites.create(location: "Chicago")
+      favorite2 = user.favorites.create(location: "LA")
+      binding.pry 
+      get '/api/v1/favorites?api_key=testkey'
+
+      favorites = JSON.parse(response.body)
+
+
     end
   end
 
